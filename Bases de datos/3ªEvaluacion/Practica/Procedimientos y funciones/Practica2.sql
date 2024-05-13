647a05f24ea56034cf461138738376ae82a0509e
Anxo Campos Carballo <anxo.campos260204@gmail.com>
2024-05-13 19:33:15 +0200

    all

--- Bases de datos/3ªEvaluacion/Practica/Procedimientos y funciones/Practica2.sql
+++ Bases de datos/3ªEvaluacion/Practica/Procedimientos y funciones/Practica2.sql
@@ -1,29 +1,38 @@
-#Crea una función que devuelva la suma de dos números.
+#1 Crea una función que devuelva la suma de dos números.
 SELECT suma(3,5) AS suma;
 
-#Escribe un procedimiento almacenado que multiplique dos números y los muestre como resultado.
+#2 Escribe un procedimiento almacenado que multiplique dos números y los muestre como resultado.
 CALL multiplicacion(3,5);
 
-#Implementa un bucle que muestre los números del 1 al 10.
+#3 Implementa un bucle que muestre los números del 1 al 10.
 CALL mostrar_numeros();
 
 
-#Crea una función que calcule el factorial de un número dado.
+#4 Crea una función que calcule el factorial de un número dado.
 SELECT factorial_de_un_numero(5) AS "Resultado";
 
-#Diseña un procedimiento almacenado que genere una tabla con los primeros 10 números pares.
+#5 Diseña un procedimiento almacenado que genere una tabla con los primeros 10 números pares.
 
 
-#Utiliza un bucle para mostrar los números impares del 1 al 20.
+#6 Utiliza un bucle para mostrar los números impares del 1 al 20.
+
+WITH RECURSIVE OddNumbers AS (
+  SELECT 1 AS Number
+  UNION
+  SELECT Number + 2 AS Number
+  FROM OddNumbers
+  WHERE Number < 19
+)
+
+SELECT Number
+FROM OddNumbers;
+#7 Implementa una función que devuelva el cuadrado de un número.
+SELECT cuadrado_de_un_numero(5);
+
+#8 Escribe un procedimiento almacenado que calcule el promedio de una lista de números.Crea un bucle que muestre la tabla de multiplicar de un número específico.
 
 
-#Implementa una función que devuelva el cuadrado de un número.
+#9 Diseña una función que determine si un número dado es primo.
 
 
-#Escribe un procedimiento almacenado que calcule el promedio de una lista de números.Crea un bucle que muestre la tabla de multiplicar de un número específico.
-
-
-#Diseña una función que determine si un número dado es primo.
-
-
-#Implementa un procedimiento almacenado que calcule la suma de los números pares del 1 al 100.
+#10 Implementa un procedimiento almacenado que calcule la suma de los números pares del 1 al 100.
