3b15099b85c6a24a1bb60f48fa1740a5c1b90606
Anxo Campos <anxo.campos260204@gmail.com>
2024-05-27 16:39:27 +0200

    practica Swing

---
+++ Programacion/3ªEvaluacion/Swing/Practica1/ListaPersonas.java
@@ -0,0 +1,37 @@
+package Swing.Practica1;
+
+import java.util.ArrayList;
+//import java.util.Vector;
+
+public class ListaPersonas {
+	
+    ArrayList<Persona> listaPersonas; // Atributo que identifica un vector de personas
+    
+    public ListaPersonas() {
+        listaPersonas = new ArrayList<Persona>(); // Crea el vector de personas
+    }
+    /**
+    * Método que permite agregar una persona al vector de personas
+    * @param p Parámetro que define la persona a agregar al vector de
+    * personas
+    */
+    public void añadirPersona(Persona p) {
+        listaPersonas.add(p);
+    }
+    /**
+    * Método que permite eliminar una persona del vector de personas
+    * @param i Parámetro que define la posición a eliminar en el vector
+    * de personas
+    */
+    public void eliminarPersona(int i) {	
+        if(i<listaPersonas.size())
+            listaPersonas.remove(i);
+    }
+    /**
+    * Método que permite eliminar todos los elementos del vector de
+    * personas
+    */
+    public void borrarLista() {
+    listaPersonas.removeAll(listaPersonas);
+    }
+    }
