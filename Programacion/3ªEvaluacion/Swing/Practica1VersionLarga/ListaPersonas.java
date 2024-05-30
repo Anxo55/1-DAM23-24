6b3678e4219f0e50b6813c17c5bf86a33bb590fa
Anxo55 <anxo.campos260204@gmail.com>
2024-05-30 15:27:27 +0200

    practica

---
+++ Programacion/3ªEvaluacion/Swing/Practica1VersionLarga/ListaPersonas.java
@@ -0,0 +1,87 @@
+package Swing.Practica1VersionLarga;
+
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.util.ArrayList;
+
+public class ListaPersonas {
+
+	ArrayList<Persona> listaPersonas; // Atributo que identifica un vector de personas
+
+	public ListaPersonas() {
+		listaPersonas = new ArrayList<Persona>(); // Crea el vector de personas
+	}
+
+	/**
+	 * Método que permite agregar una persona al vector de personas
+	 * 
+	 * @param p Parámetro que define la persona a agregar al vector de personas
+	 */
+	public void añadirPersona(Persona p) {
+		listaPersonas.add(p);
+	}
+
+	/**
+	 * Método que permite eliminar una persona del vector de personas
+	 * 
+	 * @param i Parámetro que define la posición a eliminar en el vector de personas
+	 */
+	public void eliminarPersona(int i) {
+		if (i < listaPersonas.size())
+			listaPersonas.remove(i);
+	}
+
+	/**
+	 * Método que permite eliminar todos los elementos del vector de personas
+	 */
+	public void borrarLista() {
+		listaPersonas.removeAll(listaPersonas);
+	}
+
+	public String guardarEnArchivo(String namefichero) {
+		ArrayList<Persona> asig = this.listaPersonas;
+		String mensaje = "Error";
+		try {
+
+			FileOutputStream fileOut = new FileOutputStream(namefichero);
+			ObjectOutputStream objectOut = new ObjectOutputStream(fileOut);
+
+			objectOut.writeObject(asig);
+
+			objectOut.close();
+			fileOut.close();
+			mensaje = "Guardados en archivo correctamente.";
+
+		} catch (IOException e) {
+			mensaje = "Error al guardar en archivo: " + e.getMessage();
+
+		}
+		return mensaje;
+	}
+
+	public ArrayList<Persona> cargarDesdeArchivo(String namefichero) {
+
+		try {
+			FileInputStream fileIn = new FileInputStream(namefichero);
+			ObjectInputStream objectIn = new ObjectInputStream(fileIn);
+			//System.out.println("aqui llego");
+			this.listaPersonas = (ArrayList<Persona>) objectIn.readObject();
+
+			objectIn.close();
+			fileIn.close();
+			System.out.println("Cargados desde archivo correctamente.");
+			;
+
+		} catch (IOException | ClassNotFoundException cagada) {
+			System.out.println("Error al cargar desde archivo: " + cagada.getMessage());
+
+		}
+
+		return this.listaPersonas;
+
+	}
+
+}
---
+++ Programacion/3ªEvaluacion/Swing/Practica2/Main.java
@@ -0,0 +1,8 @@
+package Swing.Practica2;
+
+public class Main {
+    public static void main(String[] args) {
+        VentanaPrincipal ventana = new VentanaPrincipal();
+        ventana.setVisible(true);
+    }
+}
